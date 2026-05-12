33e7828e51bf1cadb7043a0e58079c131e53f96b
buikhanhhoang2003-cyber <buikhanhhoang2003@gmail.com>
2026-05-12 16:51:16 +0700

    Refactor argument parsing functions in main.c

--- main.c
+++ main.c
@@ -1,72 +1,49 @@
 #include <stdio.h>
 
+static char *skip_spaces(char *p)
+{
+    while (*p == ' ') p++;
+    return p;
+}
+
+static char *read_token(char *p, char **start_out, int *err)
+{
+    char delim = ' ';
+
+    if (*p == '"') {
+        delim = '"';
+        p++;
+    }
+
+    *start_out = p;
+
+    while (*p != '\0' && *p != delim) p++;
+
+    if (delim == '"' && *p != '"') {
+        *err = 1;
+        return p;
+    }
+
+    if (*p != '\0') {
+        *p = '\0';
+        p++;
+    }
+    return p;
+}
+
 int parse_args(char *input, char **argv, int max_args)
 {
     char *p = input;
     int argc = 0;
+    int err = 0;
 
-    if (input == NULL || argv == NULL || max_args <= 0) {
-        return 0;
+    while (1) {
+        p = skip_spaces(p);
+        if (*p == '\0' || argc >= max_args) break;
+
+        p = read_token(p, &argv[argc], &err);
+        if (err) return -1;
+        argc++;
     }
-
-    while (*p != '\0') {
-        while (*p == ' ') {
-            p++;
-        }
-
-        if (*p == '\0' || argc >= max_args) {
-            break;
-        }
-
-        if (*p == '\"') {
-            p++;
-            argv[argc] = p;
-            argc++;
-
-            while (*p != '\0' && *p != '\"') {
-                p++;
-            }
-
-            if (*p != '\"') {
-                return -1;
-            }
-
-            *p = '\0';
-            p++;
-        } else {
-            argv[argc] = p;
-            argc++;
-
-            while (*p != '\0' && *p != ' ') {
-                p++;
-            }
-
-            if (*p == ' ') {
-                *p = '\0';
-                p++;
-            }
-        }
-    }
-
     return argc;
-}
-
-int main(void)
-{
-    char input[] = "run -f \"hello world.txt\" --count 10";
-    char *argv[10];
-
-    int argc = parse_args(input, argv, 10);
-
-    if (argc < 0) {
-        printf("parse error: unclosed quote\n");
-        return 1;
-    }
-
-    printf("argc = %d\n", argc);
-    for (int i = 0; i < argc; i++) {
-        printf("argv[%d] = %s\n", i, argv[i]);
-    }
-
-    return 0;
 }
